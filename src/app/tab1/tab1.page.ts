bf8f9d3fd1cf5ebb690c46f446cd347f9b86c08d
Olga Belmones <obelmontes@am.com.mx>
2023-06-08 17:06:40 -0600

    Fin seccion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+ ngOnInit() {
+   let numero = 1;
+   numero = 2;
+   debugger;
+   numero = 3;
 
+   console.log(numero);
+ }
 }
